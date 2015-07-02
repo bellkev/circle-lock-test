4c3f3e112e5931c873a7b9af9028af89cccb4984
Kevin Bell <kevin.a.bell@gmail.com>
2015-07-02 14:20:25 -0700

    should_skip function

--- test.sh
+++ test.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bats
 
+
 setup() {
     source do-exclusively
 }
 
 @test "pargse_args branch" {
     parse_args --branch "crazy branchname"
-    [[ "$branch" == "crazy branchname" && -z "${tag+x}" && -z "${rest[0]+x}"]]
+    [[ "$branch" == "crazy branchname" && -z "${tag+x}" && -z "${rest[0]+x}" ]]
 }
 
 @test "pargse_args commit tag" {
     parse_args --tag "crazy tag"
-    [[ -z "${branch+x}" && "$tag" == "crazy tag" && -z "${rest[0]+x}"]]
+    [[ -z "${branch+x}" && "$tag" == "crazy tag" && -z "${rest[0]+x}" ]]
 }
 
 @test "pargse_args branch and tag and rest" {
@@ -21,5 +22,33 @@ setup() {
 
 @test "parse_args empty" {
     parse_args
-    [[ -z "${branch+x}" && -z "${tag+x}" == "crazy tag" && -z "${rest[0]+x}"]]
+    [[ -z "${branch+x}" && -z "${tag+x}" && -z "${rest[0]+x}" ]]
+}
+
+@test "skip if wrong branch" {
+    branch=foo
+    CIRCLE_BRANCH=bar
+    should_skip
+}
+
+@test "don't skip if right branch" {
+    branch=foo
+    CIRCLE_BRANCH=foo
+    ! should_skip
+}
+
+@test "skip if wrong tag" {
+    tag=foo
+    commit_message="No tag"
+    should_skip
+}
+
+@test "don't skip if right tag" {
+    tag=foo
+    commit_message="Has [foo] tag"
+    ! should_skip
+}
+
+@test "don't skip if branch/tag unset" {
+    ! should_skip
 }
